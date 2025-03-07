dcea0c1bb795a7f988e243b8da1a786d106455cf
Rawsen Weideman <142469376+rlw332@users.noreply.github.com>
2025-03-07 00:39:23 -0500

    Update process.c

--- process.c
+++ process.c
@@ -1,5 +1,5 @@
-#include "3140_concur.h"
-#include <stdlib.h>
-#include <MKL46Z6.h>
+#include "3140_concur.h"  //Gives declarations for the functions implemented in the header file
+#include <stdlib.h>       //Allows the use of malloc and free functions
+#include <MKL46Z6.h>      //Allows interaction with the peripherals
 
 //We have to code this part
