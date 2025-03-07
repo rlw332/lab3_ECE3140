2b7267d10060c96c24d0864b2e296cb85dbbf86c
Rawsen Weideman <142469376+rlw332@users.noreply.github.com>
2025-03-07 00:33:10 -0500

    Create process.h

---
+++ process.h
@@ -0,0 +1,29 @@
+/*
+ * process.h
+ *
+ * Nils Napp
+ * Cornell University
+ * All right reserved
+ *
+ * Jan 2024
+ * Ithaca NY
+ *
+ * This file is part of the ECE3140/CS3420 offering for Spring 2024. If you are not part
+ * of this class you should not have access to this file. Do not post, share, or otherwise
+ * distribute this file. We will consider it an AI violation if you do.
+ */
+
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+struct process_state{
+	//
+	//YOU NEED TO PUT OTHER DATA HERE
+	//
+	struct process_state * next; // keep this definition for en/de-queue
+};
+
+typedef struct process_state process_t;
+
+
+#endif /* PROCESS_H_ */
