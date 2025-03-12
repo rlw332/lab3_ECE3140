7b47ea780d4026b05327eadaea285e67a3f5ba8a
Rawsen Weideman <142469376+rlw332@users.noreply.github.com>
2025-03-12 19:24:49 -0400

    Create mytest_1.c

---
+++ mytest_1.c
@@ -0,0 +1,33 @@
+#include "3140_concur.h"
+#include "led.h"
+
+void p1 (void)
+{
+	int i;
+	for (i=0; i < 20; i++) {  //Make the process run for longer
+		delay(500);
+    	red_toggle_frdm();
+	}
+}
+
+
+int main (void){
+  led_init();
+  set15MHz();
+  set15MHz();
+
+ if (process_create (p1,32) < 0) {
+ 	return -1;
+ }
+
+ process_start ();
+
+ /*
+After process_start p1 and p2 should be running concurrently.
+Both LEDs should blink. The process with the red LED should
+finish first, and after that the green LED should blink a few
+more times, but twice as fast. Why?
+*/
+
+ return 0;
+}
