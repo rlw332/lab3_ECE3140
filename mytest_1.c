6fdfbede78fd1b04b56f9a98880cca4d65bdfb75
Rawsen Weideman <142469376+rlw332@users.noreply.github.com>
2025-03-12 20:36:50 -0400

    Update mytest_1.c

--- mytest_1.c
+++ mytest_1.c
@@ -7,16 +7,15 @@
 void p1 (void)
 {
 	int i;
-	for (i=0; i < 20; i++) {  //Make the process run for longer
+	for (i=0; i < 20; i++) {  //Make the process run for longer by changing the amount of times toggled to 20
 		delay(500);
-    	red_toggle_frdm();
+    	red_toggle_frdm();        //Could have changed to green_toggle_frdm to toggle the green LED, but the process is the same
 	}
 }
 
 
 int main (void){
   led_init();
-  set15MHz();
   set15MHz();
 
  if (process_create (p1,32) < 0) {
@@ -26,8 +25,7 @@ int main (void){
  process_start ();
 
  /*
-After process_start p1 should run. The process p1 is with the red LED should
-finish first.
+After process_start p1 should run. The process p1 is with the red LED.
 */
 
  return 0;
