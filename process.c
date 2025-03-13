e985ae1babb2e6a277217278c0868bfc30adcb90
Timothy Li <143044585+tjl243@users.noreply.github.com>
2025-03-12 20:33:27 -0400

    Update process.c

--- process.c
+++ process.c
@@ -14,29 +14,28 @@
 
 
 int process_create(void (*f)(void), int n) {
-	process_t *temp = (process_t *)malloc(sizeof(process_t));
-	if(temp == NULL){
+	process_t *temp = (process_t *)malloc(sizeof(process_t)); // create temp process that will take on input parameter values and be enqueued
+	if(temp == NULL){ // case where things fail
 		return -1;
 	}
-	__disable_irq();
-	temp -> sp = process_stack_init(f, n);
+	__disable_irq(); // process_stack_init only works when disable interrupts, this line is meaningless when interrupts are not set up
+	temp -> sp = process_stack_init(f, n); // initialize process stack, set our temp stack pointer as this initial value
 	__enable_irq();
-	temp -> n = n;
-	if(temp -> sp == NULL) {
+	temp -> n = n; // save n for later when need to free process_stack
+	if(temp -> sp == NULL) { // other case when things fail
 		free(temp);
 		return -1;
 	}
 
 	temp -> next = NULL;
-	enqueue(temp, &process_queue);
+	enqueue(temp, &process_queue); // put this process onto the queue
 	return 0;
 }
 
 void process_start (void) {
-    //same as part 3
     SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
     PIT->MCR = 0x00;
-    PIT->CHANNEL[0].LDVAL = 150000;
+    PIT->CHANNEL[0].LDVAL = 150000; // 100 Hz
 
     PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK; // enable interrupts
     NVIC_EnableIRQ(PIT_IRQn); // enable PIT interrupt
